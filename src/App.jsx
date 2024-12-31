3d2f3ed2bd59c21fe71cc9c20aa8323dbc82a181
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2024-12-31 18:52:17 +0800

    Change the output file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import About from "./pages/About";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import HowToAppoint from "./pages/HowToAppoint";
-import LandingPage from "./pages/LandingPage";
-import FAQs from "./pages/FAQs";
-import Contact from "./pages/Contact";
-import HomePage from "./pages/HomePage";
-import Home from "./pages/HomePage";
+import Guidelines from "./pages/Guidelines";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
-      <HowToAppoint />
+      <Guidelines />
       <Footer />
     </BrowserRouter>
   );
