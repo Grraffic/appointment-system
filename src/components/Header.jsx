ccbc933e7e05baf402f09c29adc27ed26b7a8b71
Rodolfo Ebajan Jr. <rodolfojrxgt@gmail.com>
2024-12-31 18:57:27 +0800

    FInal changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,76 +1,53 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Header = () => {
-  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("/home");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
 
   return (
-    <>
-      <nav className="flex justify-between items-center bg-[#eef2f7] py-4 px-20 max-w-[1440px] h-[80px] mx-auto">
-        <div className="flex items-center">
-          <img src="/image/LOGO.png" alt="LOGO" className="h-[75px]" />
-          <h1 className="text-[26px] font-tolkien text-[#252f6a] ml-4">
-            LVCC <span className="font-lato">AppointEase</span>
-          </h1>
-        </div>
-        <ul className="flex space-x-8 items-center">
-          {/* HOME */}
-          <li
-            className={`hover:bg-gray-300 px-2 ${
-              location.pathname === "/home"
-                ? "border-b-4 border-orange-500 "
-                : ""
-            }`}
-          >
-            <Link to="/home">HOME</Link>
-          </li>
+    <main className="bg-Bbackground py-4">
+      <div className="max-w-[1440px] h-14 mx-auto w-full flex justify-between items-center px-4">
+        <h2 className="flex items-center">
+          <img
+            src="/src/assets/image/LV_Logo.png"
+            alt="LV logo"
+            className="w-16 h-16 mr-2 mb-1"
+          />
+          <span className="font-regular text-2xl text-LBackground font-tolkien">
+            LVCC
+          </span>
+          <span className="font-Lato-Bold text-2xl text-LBackground pl-2 mb-1">
+            AppointEase
+          </span>
+        </h2>
 
-          {/* ABOUT */}
-          <li
-            className={`hover:bg-gray-300 px-2 ${
-              location.pathname === "/aboutus"
-                ? "border-b-4 border-orange-500 "
-                : ""
-            }`}
-          >
-            <Link to="/aboutus">ABOUT</Link>
-          </li>
+        <nav>
+          <ul className="flex space-x-10 text-[#000] relative left-40">
+            {["/home", "/about", "/faqs", "/contact"].map((link) => (
+              <li key={link} className="text-lg font-LatoRegular">
+                <Link
+                  to={link}
+                  onClick={() => handleLinkClick(link)}
+                  className={`hover:text-LBackground ${
+                    activeLink === link ? "border-b-2 border-orange-500" : ""
+                  }`}
+                >
+                  {link.replace("/", "").toUpperCase()}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
-          {/* FAQs */}
-          <li
-            className={`hover:bg-gray-300 px-2 ${
-              location.pathname === "/faqs"
-                ? "border-b-4 border-orange-500 "
-                : ""
-            }`}
-          >
-            <Link to="/faqs">FAQs</Link>
-          </li>
-
-          {/* CONTACT */}
-          <li
-            className={`hover:bg-gray-300 px-2 ${
-              location.pathname === "/contact"
-                ? "border-b-4 border-orange-500 "
-                : ""
-            }`}
-          >
-            <Link to="/contact">CONTACT</Link>
-          </li>
-
-          {/* APPOINT NOW */}
-          <li
-            className={`bg-primary text-white p-3 rounded-md hover:bg-blue-500 ${
-              location.pathname === "/howtoappoint"
-                ? "border-b-4 border-orange-500 "
-                : ""
-            }`}
-          >
-            <Link to="/howtoappoint">APPOINT NOW</Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+        <button className="px-6 py-3 bg-[#252F6A] text-[#FAFAFA] text-sm uppercase rounded-[10px] hover:bg-blue-700 relative right-3">
+          Appoint Now
+        </button>
+      </div>
+    </main>
   );
 };
 
--- src/pages/Guidelines.jsx
+++ src/pages/Guidelines.jsx
@@ -1,6 +1,5 @@
 import Buttons from "../components/Buttons";
 import Documents from "../components/Documents";
-import DocumentsStatic from "../components/DocumentsStatic";
 const Guidelines = () => {
   return (
     <div className="max-w-[1440px] mx-auto font-lato">
@@ -167,7 +166,7 @@ const Guidelines = () => {
           </h2>
           <div className="w-[400px] h-1 bg-[#F3BC62] mx-auto my-8"></div>
         </div>
-        <DocumentsStatic />
+        <Documents />
       </div>
     </div>
   );
